dbb4c9d6bd409e13766bae4f63d1a44f0f9964ba
BasilSabu <141615788+BasilSabu1@users.noreply.github.com>
2025-04-10 14:41:10 +0530

    solved error in build

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,4 +1,33 @@
-import { API_URLS } from "./components/apiconfig/api_urls";
+// import { API_URLS } from "./components/apiconfig/api_urls";
+// import axiosInstance from "./components/apiconfig/axios";
+
+// export default async function sitemap() {
+//     const baseUrl = "https://www.prepacademy.in";
+
+//     interface BlogPost {
+//       id: string | number;
+//       // Add other properties your blog posts have if needed
+//     }
+  
+  
+//     const blogResponse = await axiosInstance.get(API_URLS.BLOG.GET_BLOG);
+  
+//     const blogPost = blogResponse.data.map((post:BlogPost) => {
+//       return{
+//           url: `https://www.prepacademy.in/blogdetails/${post.id}`
+//       }
+//     })
+  
+//     return [
+//       {
+//         url: baseUrl,
+//         lastModified: new Date(),
+//       },
+//       ...blogPost
+//     ];
+//   }
+
+  import { API_URLS } from "./components/apiconfig/api_urls";
 import axiosInstance from "./components/apiconfig/axios";
 import { MetadataRoute } from 'next';
 
