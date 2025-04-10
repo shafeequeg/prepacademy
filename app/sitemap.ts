cd49086d295e1c4678fb9aed0e1930254819de94
BasilSabu <141615788+BasilSabu1@users.noreply.github.com>
2025-04-10 13:50:44 +0530

    working on sitemap file

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,28 +1,34 @@
 import { API_URLS } from "./components/apiconfig/api_urls";
 import axiosInstance from "./components/apiconfig/axios";
+import { MetadataRoute } from 'next';
 
-export default async function sitemap() {
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const baseUrl = "https://www.prepacademy.in";
-
+  
     interface BlogPost {
       id: string | number;
-      // Add other properties your blog posts have if needed
     }
   
+    let blogPosts: BlogPost[] = [];
   
-    const blogResponse = await axiosInstance.get(API_URLS.BLOG.GET_BLOG);
+    try {
+      const response = await axiosInstance.get(API_URLS.BLOG.GET_BLOG);
+      blogPosts = response.data;
+    } catch (error) {
+      console.error("Error fetching blogs for sitemap:", error);
+    }
   
-    const blogPost = blogResponse.data.map((post:BlogPost) => {
-      return{
-          url: `https://www.prepacademy.in/blogdetails/${post.id}`
-      }
-    })
+    const blogUrls = blogPosts.map((post) => ({
+      url: `${baseUrl}/blogdetails/${post.id}`,
+      lastModified: new Date(),
+    }));
   
     return [
       {
         url: baseUrl,
         lastModified: new Date(),
       },
-      ...blogPost
+      ...blogUrls,
     ];
   }
