3b608b62af6fc36ab4e0aa00f74ad68c65e3e1e0
BasilSabu <141615788+BasilSabu1@users.noreply.github.com>
2025-04-21 12:22:35 +0530

    solve error

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -25,6 +25,7 @@ export default function Header() {
   const [isLoading, setIsLoading] = useState(false);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
+  console.log(isOpen);
   
   useEffect(() => {
     const style = document.createElement('style');
